d5c8e4c84cbcae117eab41fd052a94256260026d
2300030153 <163745014+2300030153@users.noreply.github.com>
2025-07-22 23:39:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+// src/App.jsx
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutUs from './components/Aboutus';
 import Computers from './components/Computers';
 import Mobiles from './components/Mobiles';
 import Laptops from './components/Laptops';
 import Pendrives from './components/Pendrives';
-import Home from './components/Home'
-import ProductPage from './components/ProductPage'
+import Home from './components/Home';
+import ProductPage from './components/ProductPage';
 import Cart from './components/Cart';
 import Payment from './components/Payment';
 import Orders from './components/Orders';
 
-
-
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
+
   return (
-    <>
     <Router basename="/skill3">
       <Routes>
         <Route path="/" element={<HomePage />}>
@@ -41,8 +39,7 @@ function App() {
         </Route>
       </Routes>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
